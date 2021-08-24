1f414f49bdb15190fb9005ba5492ee56d61fe815
zhulos170 <89441462+zhulos170@users.noreply.github.com>
2021-08-24 13:11:39 +0700

    Update fb.py

--- fb.py
+++ fb.py
@@ -1,22 +1,3 @@
-import os
-try:
-    import requests
-except ImportError:
-    print '\n [×] Modul requests belum terinstall!...\n'
-    os.system('pip2 install requests')
-
-try:
-    import concurrent.futures
-except ImportError:
-    print '\n [×] Modul Futures belum terinstall!...\n'
-    os.system('pip2 install futures')
-
-try:
-    import bs4
-except ImportError:
-    print '\n [×] Modul Bs4 belum terinstall!...\n'
-    os.system('pip2 install bs4')
-
 import requests, os, re, bs4, sys, json, time, random, datetime
 from concurrent.futures import ThreadPoolExecutor as YayanGanteng
 from datetime import datetime
